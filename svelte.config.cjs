787cadb9d4decb218847293f2c06a3b068883336
Abel de Bruijn <A.deBruijn-3@student.tudelft.nl>
2021-05-03 22:49:43 +0200

    publish to netlify

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,7 +1,11 @@
 /** @type {import('@sveltejs/kit').Config} */
+
+const adapter = require('@sveltejs/adapter-netlify');
+
 module.exports = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
+		adapter: adapter(),
 		target: '#svelte'
 	}
 };
